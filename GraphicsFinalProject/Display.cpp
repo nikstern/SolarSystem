85255c8cfdc8f614cea92d3122fd5198465c47f8
Nick Stern <nicholas.dolan-stern630@myci.csuci.edu>
2015-11-29 16:11:28 -0800

    Final Commit, got rid of most of the unecessary code and files, changed structure of display, added code to display planets into Planets.h, redid texture initialization

--- GraphicsFinalProject/Display.cpp
+++ GraphicsFinalProject/Display.cpp
@@ -3,277 +3,105 @@
 //  GraphicsFinalProject
 //
 //  Created by Nick Stern on 11/18/15.
-//  Copyright © 2015 Dolan-Stern, Nicholas. All rights reserved.
+//  Copyright © 2015 Dolan-Stern, Nicholas. All r````ights reserved.
 //
 
 #include "Display.hpp"
-#include "Planet.h"
-#include "Texture.hpp"
-int PROGRAM;
-int POSITION;
-int NORMAL;
-int MATERIAL_A;
-int MATERIAL_D;
-int MATERIAL_S;
-int MATERIAL_SHINY;
-int LIGHT_P;
-int LIGHT_A;
-int LIGHT_D;
-int LIGHT_S;
-int LIGHT_INVERSE;
-int MODEL;
-int TEX;
-Cube CUBE = Cube();
-const size_t CUBE_POINTS_OFFSET = 0;
-const size_t CUBE_NORMALS_OFFSET = Cube::SIZE_POINTS;
-const size_t CYLINDER_POINTS_OFFSET = Cube::SIZE;
-const size_t CYLINDER_NORMALS_OFFSET = CYLINDER_POINTS_OFFSET + Cylinder::SIZE_POINTS;
-const size_t SPHERE_POINTS_OFFSET = CYLINDER_NORMALS_OFFSET+Cylinder::SIZE_NORMALS;
-const size_t SPHERE_NORMALS_OFFSET = SPHERE_POINTS_OFFSET+Sphere::SIZE_POINTS;
-const size_t SPHERE_TEXTURE_OFFSET = SPHERE_NORMALS_OFFSET+Sphere::SIZE_NORMALS;
-const size_t TOTAL_SIZE = SPHERE_NORMALS_OFFSET + Sphere::SIZE_NORMALS + Sphere::SIZE_TEXTURE;
-void DrawCube(DrawShape shape);
-void DrawCylinder(DrawShape shape);
-void DrawSphere(DrawShape shape);
-int day;
-Sphere SPHERE;
-Cylinder CYLINDER;
-void SetMaterial(Material material);
-void SetLight(Light light);
-void drawShape(DrawShape shape, int numPoints,size_t points, size_t normals);
-void drawing();
-enum { Xaxis = 0, Yaxis = 1, Zaxis = 2, NumAxes = 3 };
-int      Axis = Zaxis;
-GLfloat  Theta1[NumAxes] = { 0.0, 0.0, 0.0 };
-float r = 4.0;
-View view = View();
-Projection proj = Projection();
-mat4 matrix;
-vec4 lpos = vec4( 0.0, 0.0, 0.0, 0.0 );
-vec4 sla(1,1,1,1);
-vec4 sld(0.0, 0.0,0.0, 1.0);
-vec4 sls(0.5, 0.5, 0.5, 1.0);
-Light sunlight = Light(lpos,sla,sld,sls,false);
-vec4 la = vec4(0.1,0.1,0.1,0.0);
-vec4 ld = vec4(0.5, 0.5, 0.5, 1.0);
-vec4 ls = vec4(0.5, 0.5, 0.5, 1.0);
-Light planetlight = Light(vec4(0,0,0,0), la, ld, ls, false);
-float EARTH_SIZE = 1.0;
-float EARTH_DISTANCE = 10;
-float EARTH_THETA = 1;
-int PLANETS = 6;
-Planet planets[] = {
-    Planet(1,0,0,0),
-    Planet(EARTH_SIZE*0.3,EARTH_DISTANCE*0.387,EARTH_THETA/0.241,0.01),
-    Planet(EARTH_SIZE*0.98,EARTH_DISTANCE*0.716,EARTH_THETA/0.615,177.4),
-    Planet(EARTH_SIZE*1.0, EARTH_DISTANCE*1.0,EARTH_THETA*1.0,23.4),
-    Planet(EARTH_SIZE*0.532,EARTH_DISTANCE*1.52,EARTH_THETA/1.88,6.7),
-    Planet(EARTH_SIZE*11.21, EARTH_DISTANCE*5.2,EARTH_THETA/11.9,25.2),
-    };
-DrawShape planetShapes[] =
-{
-    DrawShape(sunlight,
-    Material(
-              vec4(1.0, 1.0, 1.0, 1.0),
-              vec4(1.0, 1.0, 1.0, 1.0),
-              vec4(1.0, 1.0, 1.0, 1.0),
-              5.0
-             ),planets[0].getSize(), vec4(1.0,1.0,0.0,1.0)),
-    DrawShape(planetlight, // MERCURY
-    Material(
-            vec4(0.0, 0.0, 0.0, 1.0),
-            vec4(1.0, 0.0, 0.0, 1.0),
-            vec4(0.3, 0.3, 0.3, 1.0),
-            5
-            ),matrix, vec4(0.0,0.0,0.0,1.0)),
-    DrawShape(planetlight, // VENUS
-    Material(
-            vec4(0.0, 0.0, 0.0, 1.0),
-            vec4(1.0, 0.0, 0.0, 1.0),
-            vec4(0.5, 1.0, 0.5, 1.0),
-            5
-            ),matrix, vec4(0.0,1.0,0.5,1.0)),
-    DrawShape(planetlight, // EARTH
-    Material(
-            vec4(0.0, 0.0, 0.0, 1.0),
-            vec4(1.0, 0.0, 0.0, 1.0),
-            vec4(0.0, 0.0, 1.0, 1.0),
-             5
-            ),matrix, vec4(0.0,0.0,1.0,1.0)),
-    DrawShape(planetlight, // MARS
-    Material(
-             vec4(0.0, 0.0, 0.0, 1.0),
-             vec4(1.0, 0.0, 0.0, 1.0),
-             vec4(1.0, 0.0, 0.0, 1.0),
-             5
-            ),matrix, vec4(1.0,0.0,0.0,1.0)),
-    DrawShape(planetlight, // JUPITER
-    Material(
-            vec4(0.0, 0.0, 0.0, 1.0),
-            vec4(1.0, 0.0, 0.0, 1.0),
-            vec4(1.0, 0.0, 0.0, 1.0),
-            5
-            ),matrix, vec4(1.0,0.5,0.0,1.0)),
-    
-};
-GLuint textures[2];
-void drawing()
-{
-    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-    GLuint PROJ = glGetUniformLocation( PROGRAM, "projection" );
-    glUniformMatrix4fv( PROJ, 1, GL_TRUE, proj.matrix);
-    GLuint VIEW = glGetUniformLocation( PROGRAM, "view" );
-glUniform1i( glGetUniformLocation(PROGRAM, "texture_on"), true );
-     glUniform1f( glGetUniformLocation(PROGRAM, "st_factor"), 10.0 );
-    glUniformMatrix4fv( VIEW, 1, GL_TRUE, view.matrix );
- //   glVertexAttribPointer( TEX, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(SPHERE_TEXTURE_OFFSET) );
-        for (int i = 0; i < PLANETS; i++)
-    {
-        
-        glBindTexture( GL_TEXTURE_2D, textures[1] );
-        Planet planet = planets[i];
-        DrawShape shape = planetShapes[i];
-        shape.setModel(Translate(planet.getX(day), 0, planet.getY(day))*Scale(planet.getSize()));
-        DrawSphere(shape);
-    }
-    
-    glutSwapBuffers();
 
-}
-void idle()
+int main(int argc, char** arv)
 {
-    day++;
-    glutPostRedisplay();
-}
-
-
-void Display(int argc, char** arv)
-{
-    day = 0;
-    glutInit(&argc,arv);
-    //glutInitDisplayMode( GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH );
-    glutInitDisplayMode(GLUT_3_2_CORE_PROFILE | GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH );
+    glutInit (&argc, arv);
+    glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGBA | GLUT_STENCIL | GLUT_DEPTH );
+    glutInitWindowPosition( 150, 150 );
     glutInitWindowSize( 800, 800 );
-    glutCreateWindow( "Color Cube" );
-    PROGRAM = InitShader( "vshader31.glsl", "fshader31.glsl" );
-    glUseProgram(PROGRAM);
-    POSITION = glGetAttribLocation( PROGRAM, "vPosition" );
-    NORMAL  = glGetAttribLocation( PROGRAM, "vNormal" );
-    MATERIAL_A = glGetUniformLocation(PROGRAM, "ka");
-    MATERIAL_D = glGetUniformLocation(PROGRAM, "kd");
-    MATERIAL_S = glGetUniformLocation(PROGRAM, "ks");
-    MATERIAL_SHINY = glGetUniformLocation(PROGRAM, "Shininess");
-    LIGHT_P = glGetUniformLocation(PROGRAM, "LightPosition");
-    LIGHT_A = glGetUniformLocation(PROGRAM, "AmbientLight");
-    LIGHT_D = glGetUniformLocation(PROGRAM, "DiffuseLight");
-    LIGHT_S = glGetUniformLocation(PROGRAM, "SpecularLight");
-    LIGHT_INVERSE = glGetUniformLocation(PROGRAM, "light_out");
-    MODEL = glGetUniformLocation(PROGRAM,"model");
-    TEX = glGetUniformLocation(PROGRAM,"vTexCoord");
-    const int  TextureSize  = 512;
-    GLubyte checker_tex[TextureSize][TextureSize][3];
-    
-    for ( int i = 0; i < TextureSize; i++ )
-    {
-        for ( int j = 0; j < TextureSize; j++ )
-        {
-            //GLubyte c = (((i & 0x4) == 0) ^ ((j & 0x1)  == 0)) * 255;
-            GLubyte c = (((j & 0x10)  == 0)) * 255;
-            checker_tex[i][j][0]  = c;
-            checker_tex[i][j][1]  = c;
-            checker_tex[i][j][2]  = c;
-        }
-    }
-    glGenTextures( 2, textures );
-    
-    glActiveTexture( GL_TEXTURE0 );
-    glBindTexture( GL_TEXTURE_2D, textures[0] );
-    glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, TextureSize, TextureSize, 0, GL_BGR, GL_UNSIGNED_BYTE, checker_tex );
-    glGenerateMipmap(GL_TEXTURE_2D);
-    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
-    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
-    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
-    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR );
+    glutCreateWindow( "Solar System" );
+    glShadeModel(GL_SMOOTH);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+    glEnable(GL_NORMALIZE);
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glClearColor(0, 0, 0, 0);
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glGenTextures( 9, textures );
     unsigned char* pic1 = NULL;
     int w,h;
-    loadBMP_custom(&pic1, &w, &h, "earth_physical.bmp");
-    glBindTexture( GL_TEXTURE_2D, textures[1] );
-    glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_BGR, GL_UNSIGNED_BYTE, pic1 );
-    glGenerateMipmap(GL_TEXTURE_2D);
-    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
-    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
-    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
-    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR );
-    
-    GLuint vao;
-    glGenVertexArrays( 1, &vao );
-    glBindVertexArray( vao );
-    GLuint buffer;
-    glGenBuffers( 1, &buffer );
-    glBindBuffer( GL_ARRAY_BUFFER, buffer );
-    glBufferData(GL_ARRAY_BUFFER, TOTAL_SIZE, NULL, GL_STATIC_DRAW);
-    glBufferSubData(GL_ARRAY_BUFFER, CUBE_POINTS_OFFSET, CUBE.SIZE_POINTS, CUBE.points);
-    glBufferSubData(GL_ARRAY_BUFFER, CUBE_NORMALS_OFFSET, CUBE.SIZE_NORMALS, CUBE.normals);
-    glBufferSubData(GL_ARRAY_BUFFER, CYLINDER_POINTS_OFFSET, CYLINDER.SIZE_POINTS, CYLINDER.points);
-    glBufferSubData(GL_ARRAY_BUFFER, CYLINDER_NORMALS_OFFSET, CYLINDER.SIZE_NORMALS, CYLINDER.normals);
-    glBufferSubData(GL_ARRAY_BUFFER, SPHERE_POINTS_OFFSET, SPHERE.SIZE_POINTS, SPHERE.points);
-    glBufferSubData(GL_ARRAY_BUFFER, SPHERE_NORMALS_OFFSET, SPHERE.SIZE_NORMALS, SPHERE.normals);
-     glBufferSubData(GL_ARRAY_BUFFER, SPHERE_TEXTURE_OFFSET, SPHERE.SIZE_TEXTURE, SPHERE.texture);
-    glEnableVertexAttribArray( POSITION );
-    glVertexAttribPointer( POSITION, 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(CUBE_POINTS_OFFSET) );
-    glEnableVertexAttribArray( NORMAL );
-    glVertexAttribPointer( NORMAL, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(CUBE_NORMALS_OFFSET) );
-    glEnable( GL_DEPTH_TEST );
-    glEnableVertexAttribArray( TEX );
-    glVertexAttribPointer( TEX, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(SPHERE_TEXTURE_OFFSET) );
-    glUniform1i( glGetUniformLocation(PROGRAM, "myTextureSampler"), 0 );
-    glUniform1i( glGetUniformLocation(PROGRAM, "texture_on"), true );
-
-    glClearColor( 1.0, 1.0, 1.0, 1.0 );
-    GLuint VIEW = glGetUniformLocation( PROGRAM, "view" );
-    glUniformMatrix4fv( VIEW, 1, GL_TRUE, view.matrix);
+    setTextures(9);
     glutDisplayFunc(drawing);
-    glutIdleFunc(idle);
+    glutTimerFunc( 20, timer, 1 );
+    glutKeyboardFunc( key );
     glutMainLoop();
+    return 0;
 }
 
-
-void SetMaterial(Material material)
+void drawing()
 {
-    glUniform4fv( MATERIAL_A, 1, material.atmos );
-    glUniform4fv( MATERIAL_D, 1, material.diffuse);
-    glUniform4fv( MATERIAL_S, 1, material.specular );
-    glUniform1f( MATERIAL_SHINY, material.shiny );
-}
-void SetLight(Light light)
-{
-    glUniform4fv( LIGHT_P, 1, light.pos );
-    glUniform4fv( LIGHT_A, 1, light.atmos );
-    glUniform4fv( LIGHT_D, 1, light.diffuse);
-    glUniform4fv( LIGHT_S, 1, light.specular );
-    glUniform1f( LIGHT_INVERSE, light.inverse );
-}
-void drawShape(DrawShape shape, int numPoints, size_t points, size_t normals)
-{
-    SetLight(shape.light);
-    SetMaterial(shape.material);
-    glUniformMatrix4fv( MODEL, 1, GL_TRUE, shape.model);
-    glUniform4fv( glGetUniformLocation(PROGRAM, "obj_color"), 1,  shape.color);
-    glVertexAttribPointer( POSITION, 4, GL_FLOAT, GL_FALSE, 0,  BUFFER_OFFSET(points));
-    glVertexAttribPointer( NORMAL, 3, GL_FLOAT, GL_FALSE, 0,  BUFFER_OFFSET(normals));
-    glDrawArrays( GL_TRIANGLES, 0, numPoints );
-}
-void DrawCube(DrawShape shape)
-{
-    drawShape(shape, CUBE.POINTS,CUBE_POINTS_OFFSET, CUBE_NORMALS_OFFSET);
-}
-void DrawCylinder(DrawShape shape)
-{
-//    drawShape(shape, CYLINDER.POINTS,CYLINDER_POINTS_OFFSET, CYLINDER_NORMALS_OFFSET);
-}
-void DrawSphere(DrawShape shape)
-{
-   drawShape(shape,SPHERE.POINTS,SPHERE_POINTS_OFFSET, SPHERE_NORMALS_OFFSET);
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT,SUNLIGHT_MODEL_AMBIENT);
+    glEnable(GL_LIGHTING);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(50, 1.5, 1.5, 10000.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT| GL_STENCIL_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(EYE1,
+              EYE2,
+              EYE3,
+              0, 0, 0,
+              0.0, 1.0, 0.020);
+    drawAllPlanets();
+    glDisable(GL_LIGHTING);
+    glutSwapBuffers();
+    glFlush();
+    
 }
 
+void key(unsigned char pressedKey, int mouseXPosition, int mouseYPosition)
+{
+    switch(pressedKey)
+    {
+        case 'a':
+            increment *= 1.1;
+            if (increment > 100.0)
+                increment = 100.0;
+            break;
+        case 'd':
+            increment *= 0.9;
+            if (increment < 0.001)
+                increment = 0.001;
+            break;
+            
+        case '1':
+            if (EYE2 > 0.01)
+                EYE2 -= 0.1;
+            break;
+            
+        case '2':
+            if (EYE2 < 100)
+                EYE2 += 0.1;
+            break;
+        case '`':
+            exit(0);
+            
+    }
+}
+
+void timer(int value)
+{
+    theta += increment;
+    days += increment;
+    glutPostRedisplay();
+    glutTimerFunc(20, timer, 1);
+}
+
+void setTextures(int num)
+{
+    RGBpixmap pix;
+    for (int i = 0; i < num; i++)
+    {
+        pix.readBMPFile(to_string(i)+".bmp", false);
+        pix.setTexture(textures[i]);
+    }
+}
 
--- GraphicsFinalProject/Planet.h
+++ GraphicsFinalProject/Planet.h
@@ -13,69 +13,252 @@
 #include <iostream>
 #include <cmath>
 #include <math.h>
+#include <GLUT/glut.h>
 using namespace std;
+
+class Satellite
+{
+public:
+    GLfloat radius;
+    GLfloat orbitRadius;
+    GLfloat orbitDuration;
+    GLfloat rotationDuration;
+    GLfloat inclination;
+    GLint num;
+    Satellite()
+    {
+        
+    }
+    Satellite(GLfloat r, GLfloat oR, GLfloat oD, GLfloat i, GLint n)
+    {
+        radius = r;
+        orbitRadius = oR;
+        orbitDuration = oD;
+        inclination = i;
+        num = n;
+    }
+};
+class Ring {
+public:
+    GLdouble inner;
+    GLdouble outer;
+    GLint slices;
+    GLint loops;
+    GLuint texture_num;
+    Ring()
+    {
+        inner = 0;
+        outer = 0;
+        slices = 0;
+        loops = 0;
+        texture_num = 0;
+    }
+    Ring(GLdouble i, GLdouble radius, GLint s, GLint l, GLuint t)
+    {
+        inner = i;
+        outer = i+radius;
+        slices = s;
+        loops = l;
+        texture_num = t;
+    }
+};
 class Planet {
 public:
-    
-    double getSize()
+    GLfloat radius;
+    GLfloat orbitRadius;
+    GLfloat orbitDuration;
+    GLfloat rotationDuration;
+    GLfloat inclination;
+    GLint num;
+    int numChildren;
+    Planet(GLfloat r, GLfloat oR, GLfloat oD, GLfloat rD, GLfloat i, GLint n)
     {
-        return size;
+        radius = r;
+        orbitRadius = oR;
+        orbitDuration = oD;
+        rotationDuration = rD;
+        inclination = i;
+        num = n;
+        numChildren = 0;
     }
-    double getDistance()
+    Planet(GLfloat r, GLfloat oR, GLfloat oD, GLfloat rD, GLfloat i, GLint n, int numChilds)
     {
-        return distance;
-    }
-    double getTheta()
-    {
-        return theta;
-    }
-    double getTilt(int day)
-    {
-        double temp = day * tilt * M_PI/180.00;
-        return temp;
-    }
-    
-    double getX(int day)
-    {
-        double temp = day * theta * M_PI/180.00;
-        
-        return distance * cos(temp);
-    }
-    double getY(int day)
-    {
-        double temp = day * theta * M_PI/180.00;
-        return distance * sin(temp);
-    }
-    Planet(double size, double distance, double theta,double til)
-    {
-        cout << "Planet is being created" << endl;
-        setSize(size);
-        setDistance(distance);
-        setTheta(theta);
-        setTilt(tilt);
-    }
-private:
-    double size;
-    double distance;
-    double theta;
-    double tilt;
-    double coeff;
-    void setSize(double s)
-    {
-        size = s;
-    }
-    void setDistance(double d)
-    {
-        distance = d;
-    }
-    void setTheta(double t)
-    {
-        theta = t;
-    }
-    void setTilt(double til)
-    {
-        tilt = til;
+        radius = r;
+        orbitRadius = oR;
+        orbitDuration = oD;
+        rotationDuration = rD;
+        inclination = i;
+        num = n;
+        numChildren = numChilds;
     }
 
 };
+
+const Ring SATURN_RING1 = Ring(10,1,48,48,1);
+const Ring SATURN_RING2 = Ring(11.3,1,48,48,2);
+const Ring SATURN_RING3 = Ring(12.5,1,48,48,3);
+const Ring SATURN_RING4 = Ring(13.7,1,48,48,4);
+const Ring SATURN_RING5 = Ring(14.9,1,48,48,5);
+const Ring JUPITER_RING1 = Ring(13.5,2.5,48,48,1);
+const Ring JUPITER_RING2 = Ring(17.05,4.0,48,48,2);
+const Ring URANUS_RING1 = Ring(5,1.0,48,48,1);
+const Ring URANUS_RING2 = Ring(6.3,2.0,48,48,2);
+const Satellite MOON = Satellite(0.27,0.257,30.0,0,0);
+const Planet SUN = Planet(1.0, 0,1,1,0,0);
+const Planet MERCURY = Planet(0.383,  0.387, 88,   59,    7, 1);
+const Planet VENUS = Planet(  0.949, 0.723, 225, -243,   3, 2);
+const Planet EARTH = Planet(  1.0, 1, 365,    1,   7, 3, 1);
+const Planet MARS = Planet(   0.532,  1.52, 686,    1,   2, 4);
+const Planet JUPITER = Planet(11.21,  5.2, 4333,   0.4, 1, 5);
+const Planet SATURN = Planet( 9.45,  9.58, 10759,  0.4, 2, 6);
+const Planet URANUS = Planet( 4.01,  19.20, 30685, -0.7, 1, 7);
+const Planet NEPTUNE = Planet(3.88,  30.05, 60190,  0.7, 2, 8);
+const GLfloat SUNLIGHT_MODEL_AMBIENT[] = { 0.2, 0.2, 0.2, 0.0};
+Planet PLANETS[9] =
+{
+    SUN,MERCURY,VENUS,EARTH,MARS,JUPITER,SATURN,URANUS,NEPTUNE
+};
+Satellite PLANET_SATELLITES[9][30]=
+{
+    {},{},{},{MOON},{},{},{},{},{},
+};
+Ring PLANET_RINGS[9][10] =
+{
+    {},{},{},{},{},{JUPITER_RING1,JUPITER_RING2},{SATURN_RING1,SATURN_RING2,SATURN_RING3,SATURN_RING4,SATURN_RING5},{URANUS_RING1,URANUS_RING1},{}
+};
+GLfloat theta = 0.00;
+GLfloat days  = 0.00;
+GLfloat increment    =10;
+const GLfloat SUNLIGHT_POSITION[]    = { 0.0, 0.0, 0.0, 1.0};
+const GLfloat SUNLIGHT_AMBIENT[]       = { 0, 0, 0, 1.0};
+const GLfloat SUNLIGHT_DIFFUSE[]       = { 1, 1, 1, 1.0};
+const GLfloat SUNLIGHT_SPECULAR[]      = { 1, 1, 1, 1.0};
+const GLfloat SUN_LIGHT_AMB[] = {1,1,1,1};
+const GLfloat SUNLIGHT_SPOT_DIRECTION[] = {0.0,0,1};
+GLuint textures[9];
+
+void sunLight();
+void updateSunLight();
+void drawAllPlanets();
+void drawPlanet(Planet planet);
+void drawRing(Ring ring, Planet planet);
+void drawSatellite(Satellite satellite, Planet planet);
+
+void sunLight()
+{
+    glEnable( GL_LIGHT0 );
+    glDisable(GL_LIGHT1);
+    glLightfv( GL_LIGHT0, GL_AMBIENT, SUNLIGHT_AMBIENT );
+    glLightfv( GL_LIGHT0, GL_DIFFUSE, SUNLIGHT_DIFFUSE );
+    glLightfv( GL_LIGHT0, GL_SPECULAR, SUNLIGHT_SPECULAR);
+    glLightfv( GL_LIGHT0, GL_POSITION, SUNLIGHT_POSITION );
+    glLightfv( GL_LIGHT0, GL_SPOT_DIRECTION, SUNLIGHT_SPOT_DIRECTION);
+    glLightf( GL_LIGHT0, GL_SPOT_EXPONENT, 0.0 );
+    glLightf( GL_LIGHT0, GL_SPOT_CUTOFF, 180.0f );
+    glLightf( GL_LIGHT0, GL_CONSTANT_ATTENUATION, 1.0 );
+    glLightf( GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.0 );
+    glLightf( GL_LIGHT0, GL_QUADRATIC_ATTENUATION, 0.0 );
+}
+
+void updateSunLight()
+{
+    glPushMatrix();
+        glLightfv(GL_LIGHT1, GL_POSITION, SUNLIGHT_POSITION);
+        glLightfv(GL_LIGHT1, GL_AMBIENT,  SUN_LIGHT_AMB);
+        glPopMatrix();
+    glEnable(GL_LIGHT1);
+}
+void drawAllPlanets()
+{
+    updateSunLight();
+    drawPlanet(SUN);
+    sunLight();
+    drawPlanet(MERCURY);
+    drawPlanet(VENUS);
+    drawPlanet(EARTH);
+    drawPlanet(MARS);
+    drawPlanet(JUPITER);
+    drawPlanet(SATURN);
+    drawPlanet(URANUS);
+    drawPlanet(NEPTUNE);
+}
+
+void drawRing(Ring ring,Planet planet)
+{
+    GLUquadricObj* quadro = gluNewQuadric();
+    gluQuadricNormals(quadro, GLU_SMOOTH);
+    gluQuadricTexture(quadro, GL_TRUE);
+    glColorMaterial ( GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE ) ;
+    glEnable(GL_COLOR_MATERIAL);
+    glColor3f(planet.num/36.0*(ring.texture_num), planet.num/36.0 *(ring.texture_num),planet.num/36.0*(ring.texture_num) );
+    glPushMatrix();
+        glRotatef(planet.inclination, 0.0, 0.0, 1.0);
+        glRotatef( 360.0 * (days/planet.orbitDuration), 0.0, 1.0, 0.0);
+        glTranslatef(planet.orbitRadius, 0.0, 0.0 );
+        glRotatef(-180/planet.num, 1.0, 0.0, 0.0 );
+        gluDisk(quadro, ring.inner/10, ring.outer/10, ring.slices,ring.loops );
+    glPopMatrix();
+    glColor3f(1, 1, 1);
+    glDisable(GL_COLOR_MATERIAL);
+    gluDeleteQuadric(quadro);
+    
+}
+void drawSatellite(Satellite satellite,Planet planet)
+{
+    GLUquadricObj* quadro = gluNewQuadric();
+    gluQuadricNormals(quadro, GLU_SMOOTH);
+    gluQuadricTexture(quadro, GL_TRUE);
+    glEnable(GL_TEXTURE_2D);
+    glPushMatrix();
+        glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+        glPushMatrix();
+            glRotatef(planet.inclination, 0.0, 0.0, 1.0);
+            glRotatef( 360.0 * (days/planet.orbitDuration), 0.0, 1.0, 0.0);
+            glTranslatef(planet.orbitRadius, 0.0, 0.0 );
+            glRotatef( 360.0 * days/satellite.orbitDuration , 0.0, 1.0, 0.0 );
+            glTranslatef( satellite.orbitRadius, 0.0, 0.0 );
+            glBindTexture(GL_TEXTURE_2D, textures[satellite.num]);
+            gluSphere(quadro, satellite.radius/10, 48, 48);
+        glPopMatrix();
+    glPopMatrix();
+    glDisable(GL_TEXTURE_2D);
+    gluDeleteQuadric(quadro);
+}
+void drawPlanet(Planet planet)
+{
+    if (planet.num !=0)
+    {
+        
+        for (int i = 0; i < 10;i++)
+        {
+            Ring ring = PLANET_RINGS[planet.num][i];
+            drawRing(ring,planet);
+        }
+        for (int i = 0; i < planet.numChildren; i++)
+        {
+            Satellite satellite = PLANET_SATELLITES[planet.num][i];
+            drawSatellite(satellite, planet);
+        }
+    }
+    GLUquadricObj* quadro = gluNewQuadric();
+    gluQuadricNormals(quadro, GLU_SMOOTH);
+    gluQuadricTexture(quadro, GL_TRUE);
+    glEnable(GL_TEXTURE_2D);
+    glPushMatrix();
+        glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+        glPushMatrix();
+				glRotatef( planet.inclination, 0.0, 0.0, 1.0);
+				glRotatef( 360.0 * (days/ planet.orbitDuration), 0.0, 1.0, 0.0);
+				glTranslatef( planet.orbitRadius, 0.0, 0.0 );
+				glRotatef( 360.0 * (theta/ planet.rotationDuration), 0.0, 1.0, 0.0 );
+                glRotatef( -90.0, 1.0, 0.0, 0.0 );
+				glBindTexture(GL_TEXTURE_2D, textures[planet.num]);
+				gluSphere(quadro, planet.radius/10, 48, 48);
+        glPopMatrix();
+    glPopMatrix();
+    glDisable(GL_TEXTURE_2D);
+    gluDeleteQuadric(quadro);
+}
+
+
 #endif /* defined(__GraphicsFinalProject__Planet__) */
